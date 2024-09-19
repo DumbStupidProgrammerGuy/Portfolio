f2705186008c5dca02fb3bee2ec6a5e04a0c5575
DumbStupidProgrammerGuy <25amcrae@nyos.org>
2024-09-19 14:27:58 +0000

    made "hi" program in java

---
+++ java/main.java
@@ -0,0 +1,9 @@
+
+//javac main.java && java main
+// replace main with file and main class name
+
+public class main {
+    public static void main ( String[] args) {
+        System.out.println("hi");
+    }
+}
